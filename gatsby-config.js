3ddeb962e6528303808d87e44b849d8239373005
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-02-05 17:52:08 -0300

    Refactor to only use published version of @vtexlab/gatsby-theme-instore-core

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Base inStore',
-  },
   plugins: [
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-offline',
     {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: 'src/images/icon.png',
-      },
-    },
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
+      resolve: '@vtexlab/gatsby-theme-instore-core',
     },
   ],
 }
