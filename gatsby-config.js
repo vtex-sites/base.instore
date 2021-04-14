6d53c4167186ad8459f66b25f809ba3fe6707c91
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-04-14 16:34:13 -0300

    Add better documentation and first setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
+const path = require('path')
+
+const STORE_ID = path.basename(__dirname).replace('.instore', '')
+
 module.exports = {
   plugins: [
     {
       resolve: '@vtexlab/gatsby-theme-instore-core',
+      options: { tenant: STORE_ID },
     },
   ],
 }
