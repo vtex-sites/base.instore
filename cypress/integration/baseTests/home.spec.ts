1ab1595e696cf2c14f090a42fc473786c8e07a09
Guilherme Bruzzi <guibruzzi@gmail.com>
2021-10-12 14:27:04 -0300

    Fix Cypress tests

--- cypress/integration/baseTests/home.spec.ts
+++ cypress/integration/baseTests/home.spec.ts
@@ -1,15 +1,21 @@
-import { loginWithRobot } from '../../utils/login'
+import { setConfig } from '../../utils/config'
+import { DEFAULT_LOGIN, loginWithRobot } from '../../utils/login'
 
 describe('home', () => {
   beforeEach(() => {
     cy.visit('/instore')
 
     cy.title().should('eq', 'VTEX inStore')
+
+    setConfig({
+      pathAfterLogin: '/',
+      vendors: { [DEFAULT_LOGIN as string]: { pathAfterLogin: '/' } },
+    })
+
+    loginWithRobot()
   })
 
   it('Verify welcome message and body', () => {
-    loginWithRobot()
-
     cy.contains('Welcome').should('be.visible')
 
     cy.contains('Rendered by Physical Store Team').should('be.visible')
